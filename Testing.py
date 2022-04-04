23de59de3049667bb97c87976a75d76e7d7aa8a1
ShahaB-AfriDy <shahabkust38@gmail.com>
2022-04-05 05:05:29 +1200

    Reset the code

--- Testing.py
+++ Testing.py
@@ -1,18 +1,5 @@
-
-<<<<<<< HEAD
-print("Equal != equal")
-
-List = [1,2,3,4,5]
-
-print(List)
-=======
-__author__ = "Equal != equal"
-
 print("Equal!=equal")
 print("Equal!=equal")
+
 print("Equal!=equal")
 print("equal != Equal")
-
-
-
->>>>>>> origin/main
