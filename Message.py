6111f833af440ae603d57ff936d83d2c7598dab9
ShahaB-AfriDy <shahabkust38@gmail.com>
2022-04-08 23:06:40 +1200

    Message file is added

---
+++ Message.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from datetime import datetime
+
+
+win = Tk()
+
+win.title("Equal != equal")
+win.geometry("500x400")
+win.config(background='steelblue')
+label = Label(win,text='Welcome!',bg='steelblue',font=('Arial',50,'bold'))
+label.pack(pady=145)
+win.mainloop()
